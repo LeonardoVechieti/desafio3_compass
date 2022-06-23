babe60abfefdf90bae722afc919fb1f7582601d1
JoaoGHenz <joao.henz.pb@compasso.com.br>
2022-06-23 14:39:27 -0300

    Feat: text

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
   <div id="app">
+    
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
+    <myText />
     <router-view/>
   </div>
 </template>
+
+<script>
+import myText from './components/myText/myText.vue'
+
+export default {
+  name: "app",
+  components: {
+    myText
+  }
+}
+
+</script>
 
 <style lang="scss">
 #app {
