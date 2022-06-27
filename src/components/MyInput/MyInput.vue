aa0aefbab6ebe04b17829bab687449b9dd016f8e
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-06-27 17:05:09 -0300

    feat: image mobile

--- src/components/MyInput/MyInput.vue
+++ src/components/MyInput/MyInput.vue
@@ -1,25 +1,41 @@
 <template>
-    <div>
-        <input 
-        :type="Type"
-        :class="classInputs"
-        :id="idInputs"
-        :placeholder="placeholder" 
-        :value="value"
-        />
-    </div>
+  <div>
+    <input
+      :type="Type"
+      :class="classInputs"
+      :id="idInputs"
+      :placeholder="placeholder"
+      :value="value"
+    />
+  </div>
 </template>
 
 <script>
 export default {
-    name: "MyInput",
-    props: {
-        Type: String,
-        classInputs: String,
-        placeholder: String,
-        idInputs: String
-    }
-}
+  name: "MyInput",
+  props: {
+    Type: {
+      type: String,
+      default: "text",
+    },
+    classInputs: {
+      type: String,
+      default: "inputs",
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    idInputs: {
+      type: String,
+      default: "",
+    },
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+};
 </script>
 
 <style lang="sass" scoped>
