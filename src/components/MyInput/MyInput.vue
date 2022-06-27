60361f0a987ea4fb219b99ca776334cddc18cbae
Joao Treto <joaoatreto@gmail.com>
2022-06-27 14:35:24 -0300

    feat: component inputs creation

--- src/components/MyInput/MyInput.vue
+++ src/components/MyInput/MyInput.vue
@@ -1,3 +1,27 @@
 <template>
-    <p>Ola Input</p>
+    <div>
+        <input 
+        :type="Type"
+        :class="classInputs"
+        :id="idInputs"
+        :placeholder="placeholder" 
+        :value="value"
+        />
+    </div>
 </template>
+
+<script>
+export default {
+    name: "MyInput",
+    props: {
+        Type: String,
+        classInputs: String,
+        placeholder: String,
+        idInputs: String
+    }
+}
+</script>
+
+<style lang="sass" scoped>
+@import './MyInput.scss'
+</style>
