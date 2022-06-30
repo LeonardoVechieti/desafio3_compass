7bfe45cc843859f995adc2998f8cf60a3a7b92b2
JoaoGHenz <joao.henz.pb@compasso.com.br>
2022-06-30 15:00:25 -0300

    Feat: Partially working vuex

--- src/components/MyInput/MyInput.vue
+++ src/components/MyInput/MyInput.vue
@@ -6,11 +6,15 @@
       :id="idInputs"
       :placeholder="placeholder"
       :value="value"
+      @input="getVal"
+      @click="submitVal"
     />
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+
 export default {
   name: "MyInput",
   props: {
@@ -34,6 +38,30 @@ export default {
       type: String,
       default: "",
     },
+  },
+  methods: {
+    ...mapActions(["setUserName", "setUserPassword", "getUserName", "getUserPassword", "isCharAt", "foo"]),
+
+    getVal(e) {
+      if(this.idInputs === "user") {
+        this.setUserName(e.target.value);
+        console.log("Username =", e.target.value)
+        return;
+      }
+      
+      if(this.idInputs === "password") {
+        this.setUserPassword(e.target.value);
+        console.log("Password =", e.target.value)
+        return;
+      }
+    },
+    
+    submitVal() {
+      if(this.Type === "submit") {
+        //TODO: Find a way to make this work
+      }
+    }
+
   },
 };
 </script>
