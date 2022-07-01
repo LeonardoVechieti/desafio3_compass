4ecd95a02de1a33e6819f58a6b4a1a3eb0682e08
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-07-01 17:44:13 -0300

    Fix: css pageHome

--- src/views/PageHome/PageHome.vue
+++ src/views/PageHome/PageHome.vue
@@ -15,48 +15,84 @@
         <h1>22°</h1>
       </div>
     </div>
-    <div class="message">
-      <MyText size="size-36px" text="Our mission is" id="red-bold" />
-      <MyText size="size-24px" text="Nossa missão é" id="black" />
-      <MyText size="size-64px" text="to transform the world" id="red-bold" />
-      <MyText size="size-24px" text="transformar o mundo" id="black" />
-      <MyText
-        size="size-64px"
-        text="building digital experiences"
-        id="red-bold"
-      />
-      <MyText
-        size="size-24px"
-        text="construindo experiências digitais"
-        id="black"
-      />
-      <MyText
-        size="size-64px"
-        text="that enable our client’s growth"
-        id="red-bold"
-      />
-      <MyText
-        size="size-24px"
-        text="que permitam o crescimento dos nossos clientes"
-        id="black"
-      />
+    <div class="container">
+      <div class="image">
+        <img src="@/assets/uol.png" alt="" />
+      </div>
+      <div class="message">
+        <MyText size="size-24px" text="Our mission is" id="red-bold" />
+        <MyText size="size-24px" text="Nossa missão é" id="black" />
+        <MyText size="size-40px" text="to transform the world" id="red-bold" />
+        <MyText size="size-20px" text="transformar o mundo" id="black" />
+        <MyText
+          size="size-40px"
+          text="building digital experiences"
+          id="red-bold"
+        />
+        <MyText
+          size="size-20px"
+          text="construindo experiências digitais"
+          id="black"
+        />
+        <MyText
+          size="size-40px"
+          text="that enable our client’s growth"
+          id="red-bold"
+        />
+        <MyText
+          size="size-20px"
+          text="que permitam o crescimento dos nossos clientes"
+          id="black"
+        />
+      </div>
     </div>
-    <img src="@/assets/uol.png" alt="">
-    <MyButton classButton="white" />
-    <MyButton classButton="dark" />
+
+    <div class="footer">
+      <div class="text1">
+        <MyText
+          size="size-13px"
+          text="Essa janela do navegador é usada para manter sua sessão de autenticação ativa. Deixe-a aberta em segundo plano e abra uma nova janela para continuar a navegar."
+        />
+        <MyText
+          size="size-13px"
+          text=" Deixe-a aberta em segundo plano e abra uma nova janela para continuar a navegar."
+        />
+      </div>
+      <div class="text2">
+        <MyText size="size-16px" text="Application refresh in" />
+      </div>
+      <div class="text3">
+        <MyText size="size-64px" text="600" />
+        <MyText size="size-16px" text="seconds" />
+      </div>
+      <div class="buttons">
+        <div class="button1" @click="continuar()">
+          <MyText size="size-16px" text="Continuar Navegando" id="black" />
+        </div>
+        <div class="button2" @click="logout()">
+          <MyText size="size-16px" text="Logout" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
-import MyButton from "@/components/MyButton/MyButton.vue";
 import MyImg from "@/components/MyImg/MyImg.vue";
 import MyText from "@/components/MyText/MyText.vue";
 
 export default {
   name: "PageHome",
   components: {
-    MyButton,
     MyImg,
     MyText,
+  },
+  methods: {
+    continuar() {
+      this.$router.push("/home");
+    },
+    logout() {
+      this.$router.push("/login");
+    },
   },
 };
 </script>
