1b9dfeccafd909782f169439f5fdeedfe869bdef
LeonardoVechieti <92764500+LeonardoVechieti@users.noreply.github.com>
2022-07-03 16:12:03 -0300

    Feat: Created component ApiWeather

---
+++ src/components/ApiWeather/ApiWeather.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div class="weather" v-if="typeof weather.main != 'undefined'">
+      <div class="location-box">
+        <div class="location">
+          {{ weather.name }} - {{ weather.sys.country }}
+        </div>
+      </div>
+      <div class="weather-box">
+        <div class="temp">{{ Math.round(weather.main.temp) }} °C</div>
+        <!-- <div class="weather">{{ weather.weather[0].main }}</div> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ApiWeather",
+  data() {
+    return {
+      api_key: "622d7ce49f9c095eb9eaf1575f2417df",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      lang: "pt_br",
+      weather: {},
+    };
+  },
+  created() {
+    this.cordenadas();
+  },
+  methods: {
+    cordenadas() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            this.fetchWeather(
+              position.coords.latitude,
+              position.coords.longitude
+            );
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      } else {
+        console.log("Geolocation is not supported by this browser.");
+      }
+    },
+
+    fetchWeather(lat, long) {
+      fetch(
+        `${this.url_base}weather?lat=${lat}&lon=${long}&lang=${this.lang}&units=metric&APPID=${this.api_key}`
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then(this.setResults);
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+    dateBuilder() {
+      let d = new Date();
+      let months = [
+        "Janeiro",
+        "Fevereiro",
+        "Março",
+        "Abril",
+        "Maio",
+        "Junho",
+        "Julio",
+        "Agosto",
+        "Setembro",
+        "Outubro",
+        "Novembro",
+        "Dezembro",
+      ];
+      let days = [
+        "Domingo",
+        "Segunda",
+        "Terça",
+        "Quarta",
+        "Quinta",
+        "Sexta",
+        "Sábado",
+      ];
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
+      return `${day} ${date} ${month} ${year}`;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "./ApiWeather.scss";
+</style>
